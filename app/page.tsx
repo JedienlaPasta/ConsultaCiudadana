4b2f285924defb0b94a3c14ce73346b2caed5d3a
Sebastián Castaneda <scastaneda@elquisco.cl>
2025-05-09 09:23:08 -0400

    videito

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,30 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flexs container mx-auto w-full flex-col items-center justify-center px-8 py-12">
-      <div className="absolute left-0 -z-10 h-56 w-full bg-[#163448]">
-        <video autoPlay src="/banner.mp4" />
+    <>
+      <div className="relative">
+        <div className="absolute top-0 left-0 -z-10 h-full w-full">
+          <span className="absolute h-full w-full bg-[#163448] opacity-25" />
+          <video
+            autoPlay={true}
+            muted={true}
+            loop={true}
+            playsInline={true}
+            src="/banner.mp4"
+            className="h-full w-full object-cover"
+            aria-label="Banner"
+          />
+        </div>
+        <div className="bg-[#163448]s container mx-auto w-full flex-col px-8 py-70">
+          <h1 className="text-5xl font-semibold text-white">
+            Consultas Ciudadanas - El Quisco
+          </h1>
+          <Link href="/consultas/piimep" className="text-white">
+            Consulta Piimep
+          </Link>
+        </div>
       </div>
-      <h1 className="text-4xl font-semibold text-[#e0e5ee]">
-        Consultas Ciudadanas - El Quisco
-      </h1>
-      <Link href="/consultas/piimep" className="text-[#e0e5ee]">
-        Consulta Piimep
-      </Link>
-    </div>
+      <div className="h-200">.</div>
+    </>
   );
 }
