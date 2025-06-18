bd03d79695289f437cb17e49f2928d9dd38139bc
Sebastián Ignacio Castaneda Brito <scastaneda@elquisco.cl>
2025-06-18 12:15:58 -0400

    survey ui changes

--- app/ui/SurveyLayout.tsx
+++ app/ui/SurveyLayout.tsx
@@ -4,7 +4,7 @@ import MapSection from "./piimep/MapSection";
 
 export default function SurveyLayout() {
   return (
-    <div className="grid grid-cols-3 gap-4 rounded-lg border-gray-200 bg-white shadow-md shadow-gray-200/80 md:border md:p-6">
+    <div className="grid grid-cols-3 gap-4 rounded-lg border-gray-200 bg-white shadow-gray-200/80 md:border md:p-6 md:shadow-md">
       <div className="col-span-3 lg:col-span-1">
         <SurveyProgress />
       </div>
