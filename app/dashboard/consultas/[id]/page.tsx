c3841fe3cbf21233878ae4527c50ddbd09f4b084
Sebastián Ignacio Castaneda Brito <scastaneda@elquisco.cl>
2025-09-10 15:31:37 -0300

    changed piechart logo

--- app/dashboard/consultas/[id]/page.tsx
+++ app/dashboard/consultas/[id]/page.tsx
@@ -405,25 +405,18 @@ export default async function SurveyDetailsOverview({ params }: PageProps) {
                   Análisis detallado de las respuestas
                 </p>
               </div>
-              <div className="rounded-lg bg-purple-50 p-2">
+              <div className="hidden rounded-lg bg-rose-100/80 p-2 md:block">
                 <svg
-                  className="h-6 w-6 text-purple-600"
-                  fill="none"
-                  stroke="currentColor"
+                  className="size-6 text-rose-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
                   viewBox="0 0 24 24"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"
-                  />
+                  <path d="M13.5 2c-.178 0-.356.013-.492.022l-.074.005a1 1 0 0 0-.934.998V11a1 1 0 0 0 1 1h7.975a1 1 0 0 0 .998-.934l.005-.074A7.04 7.04 0 0 0 22 10.5 8.5 8.5 0 0 0 13.5 2Z" />
+                  <path d="M11 6.025a1 1 0 0 0-1.065-.998 8.5 8.5 0 1 0 9.038 9.039A1 1 0 0 0 17.975 13H11V6.025Z" />
                 </svg>
               </div>
             </div>
