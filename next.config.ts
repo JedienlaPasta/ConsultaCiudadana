f74f414111e797b8dd2d36a07032ac87bafbefa6
Sebastián Ignacio Castaneda Brito <scastaneda@elquisco.cl>
2025-10-07 11:51:40 -0300

    fixed next confing warning

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,11 @@
 import type { NextConfig } from 'next';
-import type { Configuration } from 'webpack';
 
 const nextConfig: NextConfig = {
   output: "standalone",
   experimental: {
     optimizePackageImports: ["leaflet"],
-    serverComponentsExternalPackages: ["exceljs", "rimraf", "fstream"],
   },
-  serverExternalPackages: ["exceljs"],
-  webpack: (config: Configuration, { isServer }: { isServer: boolean }) => {
-    if (isServer) {
-      // Asegurar que externals existe y es un array
-      if (!config.externals) {
-        config.externals = [];
-      }
-      if (Array.isArray(config.externals)) {
-        config.externals.push("exceljs");
-      }
-    }
-    return config;
-  },
+  serverExternalPackages: ["exceljs", "rimraf", "fstream"],
 };
 
 export default nextConfig;
